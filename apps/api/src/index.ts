b1464f18fb7ae65a2e7a83f1fe99bc03d8060cb5
Presum0 <preethamnelaturi@gmail.com>
2026-09-08 14:33:53 +0530

    connected backend to frontend

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,25 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
 
 const app = Fastify({
   logger: true,
+});
+
+app.register(cors, {
+  origin: "http://localhost:5173",
 });
 
 app.get("/health", async () => {
   return { status: "ok" };
 });
 
-app.listen({ port: 3000 });
+const start = async () => {
+  try {
+    await app.listen({ port: 3000 });
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
